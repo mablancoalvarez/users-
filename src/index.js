10902e2478ee69043beee75451f8303e64006b0f
mablancoalvarez <mablancoalvarez@gmail.com>
2020-06-22 12:41:34 +0200

    add comments

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentDetail from './CommentDetail';
+
+const App = () => {
+  return (
+    <div className="ui container comments">
+    <CommentDetail />
+    </div>
+
+  )
+};
+ReactDOM.render(<App />, document.getElementById('root')
+);
+
+
